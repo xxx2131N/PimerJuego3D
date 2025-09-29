98d99d372774b121be63519fa346df9a7fb6427b
xxx2131N <inesyefra39@gmail.com>
2025-09-28 23:16:32 -0300

    Setup: Configuracion inicial del proyecto Unity con git ignore

---
+++ Assets/Prefabs/Player/Scrips/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Vector3 fuerzaPorAplicar;
+    private float tiempoDesdeUltimaFuerza;
+    private float intervaloTiempo;
+    void Start()
+    {
+        fuerzaPorAplicar = new Vector3(0, 0, 300f);
+        tiempoDesdeUltimaFuerza = 0f;
+        intervaloTiempo = 2f; 
+    }
+
+    private void FixedUpdate()
+    {
+        tiempoDesdeUltimaFuerza += Time.fixedDeltaTime;
+
+        if (tiempoDesdeUltimaFuerza >= intervaloTiempo)
+        {
+            GetComponent<Rigidbody>().AddForce(fuerzaPorAplicar);
+            tiempoDesdeUltimaFuerza = 0f;
+        }
+    }
+}
